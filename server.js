2b7b9453251d8341d8e8a07febb6c263bec0378a
APPLE <apple@brajinder.local>
2023-04-07 00:14:52 +0530

    formatting

--- server.js
+++ server.js
@@ -8,26 +8,22 @@ require('./dbConnection');
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server running excel');
-});
-
 app.get('/importToMongo', async (req, res) => {
-
     const xlFile = xlsx.readFile("./excel.xlsx");
     const sheet = xlFile.Sheets[xlFile.SheetNames[0]];
-
     const json = xlsx.utils.sheet_to_json(sheet);
-
     await excelSchema.save(json).then((result) => {
         if (result.length > 0) {
             res.send({ msg: "success" });
         }
     });
-
 })
 
 app.use('/', (aya, gaya) => {
     gaya.json({ msg: 'gaya' });
+});
+
+app.listen(3000, () => {
+    console.log('Server running excel');
 });
 
